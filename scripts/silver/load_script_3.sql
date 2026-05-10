3723ac6eb6b767efc1c1dfcc506480a9f54954ac
Arunvijay <arunvijaysnav@gmail.com>
2026-05-10 13:31:53 +0530

    Refactor sales calculations in load_script_3.sql for the silver layer table silver.crm_sales_details

--- scripts/silver/load_script_3.sql
+++ scripts/silver/load_script_3.sql
@@ -26,9 +26,15 @@ select sls_ord_num,
 			when len(sls_due_dt) != 8 then NULL
 			else cast(cast(sls_due_dt as varchar) as date) 
 	   end sls_due_dt,	   
-	   sls_sales,
+	   case when sls_sales is null or sls_sales <= 0 or sls_sales != sls_quantity * abs(sls_price)
+			then sls_quantity * abs(sls_price)
+			else sls_sales
+	   end sls_sales,
 	   sls_quantity,
-	   sls_price
+	   case when sls_price <= 0 or sls_price is null 
+			then sls_sales / nullif(sls_quantity, 0)
+			else sls_price
+	   end sls_price
 from bronze.crm_sales_details;
 
 
@@ -148,15 +154,115 @@ Exploration: bronze.crm_sales_details
 	--       * if price is negative, convert it to a positive value. 
 
 
-	select sls_sales,
+	select distinct 
+		   sls_sales as old_sls_sales,
 		   sls_quantity,
 		   sls_price as old_sls_price,
 		   case when sls_sales is null or sls_sales <= 0 or sls_sales != sls_quantity * abs(sls_price)
 					then sls_quantity * abs(sls_price)
 				else sls_sales
-		   end sls_sales
+		   end sls_sales,
+
+		   case when sls_price <= 0 or sls_price is null then sls_sales / nullif(sls_quantity, 0)
+				else sls_price
+			end sls_price
 	from bronze.crm_sales_details
 	where sls_sales != sls_quantity * sls_price or
 		  sls_sales is null or sls_quantity is null or sls_price is null
 		  or sls_sales <= 0 or sls_quantity <= 0 or sls_price <= 0
-		  order by sls_sales, sls_quantity, sls_price
+		  order by sls_sales, sls_quantity, sls_price;
+	
+
+/*
+=========================================================
+INSERTION: Inserting into silver.crm_sales_details
+=========================================================
+*/
+
+-- check the existing structure(DDL) of the silver.crm_sales_details.
+
+
+if object_id('silver.crm_sales_details', 'U') is not null
+begin 
+	DROP TABLE silver.crm_sales_details;
+
+	CREATE TABLE silver.crm_sales_details (
+		sls_ord_num     NVARCHAR(50),
+		sls_prd_key     NVARCHAR(50),
+		sls_cust_id     int,
+		sls_order_dt    date,
+		sls_ship_dt     date,
+		sls_due_dt      date,
+		sls_sales       int,
+		sls_quantity    int,
+		sls_price       int,
+		dwh_create_date datetime2 default getDate()
+	);
+end;
+
+/*
+=================
+insertion
+=================
+*/
+
+insert into silver.crm_sales_details(
+		sls_ord_num,
+		sls_prd_key,
+		sls_cust_id,
+		sls_order_dt,
+		sls_ship_dt,
+		sls_due_dt,
+		sls_sales,
+		sls_quantity,
+		sls_price
+)
+select sls_ord_num,
+	   sls_prd_key,
+	   sls_cust_id,
+	   case when sls_order_dt <= 0 then NULL
+			when len(sls_order_dt) != 8 then NULL
+			else cast(cast(sls_order_dt as varchar) as date) 
+	   end sls_order_dt,
+	   case when sls_ship_dt <= 0 then NULL
+			when len(sls_ship_dt) != 8 then NULL
+			else cast(cast(sls_ship_dt as varchar) as date) 
+	   end sls_ship_dt,
+	   case when sls_due_dt <= 0 then NULL
+			when len(sls_due_dt) != 8 then NULL
+			else cast(cast(sls_due_dt as varchar) as date) 
+	   end sls_due_dt,	   
+	   case when sls_sales is null or sls_sales <= 0 or sls_sales != sls_quantity * abs(sls_price)
+			then sls_quantity * abs(sls_price)
+			else sls_sales
+	   end sls_sales,
+	   sls_quantity,
+	   case when sls_price <= 0 or sls_price is null 
+			then sls_sales / nullif(sls_quantity, 0)
+			else sls_price
+	   end sls_price
+from bronze.crm_sales_details;
+
+/*
+===============================
+Data quality checks
+===============================
+*/
+
+select * from silver.crm_sales_details;
+
+-----------------------------------------------------------
+select sls_order_dt
+from silver.crm_sales_details
+where sls_order_dt > sls_ship_dt or sls_order_dt > sls_due_dt;-- (Data: Okay)
+
+-------------------------------------------------------------
+
+select sls_sales,
+	   sls_quantity,
+	   sls_price
+from silver.crm_sales_details
+where sls_sales != sls_quantity * sls_price or
+	  sls_sales is null or sls_quantity is null or sls_price is null
+	  or sls_sales <= 0 or sls_quantity <= 0 or sls_price <= 0
+	  order by sls_sales, sls_quantity, sls_price;
